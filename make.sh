4ba0f922dd5c6c3dc36c9563874cade195a9b2fc
dsp <you@example.com>
2018-06-28 13:26:54 +0330

    add ansroid files

---
+++ make.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+AAPT="/usr/share/android-sdk/build-tools/20.0.0/aapt"
+DX="/usr/share/android-sdk/build-tools/20.0.0/dx"
+ZIPALIGN="/usr/share/android-sdk/build-tools/20.0.0/zipalign"
+APKSIGNER="apksigner" # /!\ version 26
+PLATFORM="/usr/share/android-sdk/platforms/android-19/android.jar"
+
+echo "Cleaning..."
+rm -rf obj/*
+rm -rf src/com/example/helloandroid/R.java
+
+echo "Generating R.java file..."
+$AAPT package -f -m -J src -M AndroidManifest.xml -S res -I $PLATFORM
+
+echo "Compiling..."
+#javac -d obj -classpath src -bootclasspath $PLATFORM -source 1.7 -target 1.7 src/com/example/helloandroid/MainActivity.java
+javac -d obj -classpath src -bootclasspath $PLATFORM -source 1.7 -target 1.7 src/com/example/helloandroid/R.java
+
+echo "Translating in Dalvik bytecode..."
+$DX --dex --output=classes.dex obj
+
+echo "Making APK..."
+$AAPT package -f -m -F bin/hello.unaligned.apk -M AndroidManifest.xml -S res -I $PLATFORM
+$AAPT add bin/hello.unaligned.apk classes.dex
+
+echo "Aligning and signing APK..."
+$APKSIGNER sign --ks nd bin/hello.unaligned.apk
+$ZIPALIGN -f 4 bin/hello.unaligned.apk bin/hello.apk
+
+if [ "$1" == "test" ]; then
+	echo "Launching..."
+	adb install -r bin/hello.apk
+	adb shell am start -n com.example.helloandroid/.MainActivity
+fi
